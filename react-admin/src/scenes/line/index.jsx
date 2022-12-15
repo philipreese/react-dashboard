94b4a122f4509a12edcb50e06b0cd512854c3941
Philip Reese <philipreese@mail.com>
2022-12-14 23:51:15 -0500

    Format with prettier

--- react-admin/src/scenes/line/index.jsx
+++ react-admin/src/scenes/line/index.jsx
@@ -3,14 +3,17 @@ import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
 
 const Line = () => {
-    return (
-        <Box m="20px">
-            <Header title="Line Chart" subtitle="Simple Line Chart" />
-            <Box height="75vh">
-                <LineChart />
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box m="20px">
+      <Header
+        title="Line Chart"
+        subtitle="Simple Line Chart"
+      />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
+    </Box>
+  );
+};
 
 export default Line;
--- react-admin/src/scenes/pie/index.jsx
+++ react-admin/src/scenes/pie/index.jsx
@@ -3,14 +3,17 @@ import Header from "../../components/Header";
 import PieChart from "../../components/PieChart";
 
 const Pie = () => {
-    return (
-        <Box m="20px">
-            <Header title="Pie Chart" subtitle="Simple Pie Chart" />
-            <Box height="75vh">
-                <PieChart />
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box m="20px">
+      <Header
+        title="Pie Chart"
+        subtitle="Simple Pie Chart"
+      />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
+};
 
-export default Pie
+export default Pie;
